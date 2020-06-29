975e3e0c5ab9a0a55d66647fe829a29c8709a08c
jared2021 <jared_schoolcraft@yahoo.com>
2020-06-29 16:38:43 -0400

    Standard 'hello world' website.

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    Hello World, Welcome to our Tourism 06 website.
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
