a5ec4babc2c40b6752abb0cd3ba59f82b2fbace6
cluzadder <59350449+cluzadder@users.noreply.github.com>
2020-07-12 13:09:26 -0700

    Add files via upload

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="home">
+    <div class="background">
+      <img src="@/assets/indianapolis.jpg" style="width:100%, height:100%">
+    </div>
     <div class="container">
       <div class="content">
         <h1>Indycate</h1>
         <p>Hello and Welcome to our Tourism 06 website!</p>
-      </div>
-      <div class="search">
         <input type="text" placeholder="Search Here">
       </div>
     </div>
   </div>
 </template>
+<style>
+
+</style>
 <style>
 * {
   box-sizing: border-box;
@@ -37,17 +41,6 @@ body {
   color: Black;
   width: 100%;
   padding: 20px;
-}
-
-.container .search{
-  position: absolute;
-  top: 525px;
-  left: 850px;
-  padding: 3px;
-  border: 5px solid black;
-  font-size: 17px;
-  max-width: 200px;
-  
 }
 
 </style>
