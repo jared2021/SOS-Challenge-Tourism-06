0f634da8ab642cb6a40d99d446fd5dd02c08cc13
jared2021 <jared_schoolcraft@yahoo.com>
2020-07-20 12:12:28 -0400

    Fixed conflict with App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,8 @@
         <router-link to="/">Home</router-link> |
         <router-link to="/about">About</router-link> |
         <router-link to="/filter">Filter</router-link> |
-<<<<<<< HEAD
-        <router-link to="/nameresult">Search</router-link>
-=======
+        <router-link to="/nameresult">Search</router-link> |
         <router-link to="/allbusinesses">All Businesses</router-link>
->>>>>>> 3c09c07d41bdcf544d0e81ca7284d19006cd01e9
       </div> 
         <router-view />
   </div>
