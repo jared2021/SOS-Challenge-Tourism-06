79a2cb9a4c03656386391683a4d9b8676392169b
jared2021 <jared_schoolcraft@yahoo.com>
2020-07-20 23:09:36 -0400

    Removed age filter

--- src/views/Filter.vue
+++ src/views/Filter.vue
@@ -16,29 +16,6 @@
       <input type="checkbox" name="precautions" value="required_face_coverings" id="required_face_coverings"> Customers Required to Wear Face Coverings
       <input type="checkbox" name="precautions" value="employee_face_coverings" id="employee_face_coverings"> Employees Required to Wear Face Coverings
     </div>
-    <div class="filter2">
-      <p3>
-        Age with drop down list for options
-      </p3>
-      <div class="age">
-        <select>
-          <option value="0"> What is your age?</option>
-          <option value="1">16-20</option>
-          <option value="2">21-25</option>
-          <option value="3">26-30</option>
-          <option value="4">31-35</option>
-          <option value="5">36-40</option>
-          <option value="6">41-45</option>
-          <option value="7">46-50</option>
-          <option value="8">51-55</option>
-          <option value="9">56-60</option>
-          <option value="10">61-65</option>
-          <option value="11">66-70</option>
-          <option value="12">71-75</option>
-          <option value="13">76-80</option>
-        </select>
-      </div>
-    </div>
     <div class="filter3">
       <p4>
         Choose businesses that interest you
